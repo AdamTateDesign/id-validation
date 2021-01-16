8729d843110f4593bc68f3e6cef410dff602eaaf
AdamTateDesign <adamtate94@gmail.com>
2021-01-16 17:26:47 +0000

    First Commit
    First Commit

---
+++ ApiModels/FileValidationResult.cs
@@ -0,0 +1,20 @@
+﻿using IdValidation.Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace id_validation_app.ApiModels
+{
+    public class FileValidationResult
+    {
+        public int IdCount => Results?.Count() ?? 0;
+
+        public int InvalidCheckDigitCount => Results?.Count(x => !x.Valid && x.Reason.Equals(ValidationReasons.Checksum)) ?? 0;
+
+        public int InvalidOtherCount => Results?.Count(x => !x.Valid && x.Reason.Equals(ValidationReasons.Other)) ?? 0;
+
+        public int ValidCount => Results?.Count(x => x.Valid) ?? 0;
+        public IEnumerable<ValidationResult> Results { get; set; }
+    }
+}
---
+++ ApiModels/ValidationResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace id_validation_app.ApiModels
+{
+    public class ValidationResult
+    {
+        public string Input { get; set; }
+
+        public bool Valid { get; set; }
+
+        public string Reason { get; set; }
+    }
+}
